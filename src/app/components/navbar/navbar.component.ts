de3cb8c494743c8e64538841cf91fc338a788553
Chad Brokaw <chbr2844@macl2974.lasp.colorado.edu>
2019-03-20 17:05:56 -0600

    search and nav respond to screen width changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
-import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, OnDestroy, ViewChild} from '@angular/core';
+import {BreakpointObserver, BreakpointState} from '@angular/cdk/layout';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy{
     @ViewChild('stickyMenu') menuElement: ElementRef;
 
     fillerNav = ['Data', 'Missions', 'Tools', 'About', 'Reference', 'Science', 'Instruments'];
@@ -13,6 +14,7 @@ export class NavbarComponent {
     sticky = false;
     searchOpen = false;
     placeholder = 'Search Datasets or Missions';
+    breakpointObserverSubscription: any;
 
     @HostListener('window:scroll', ['$event'])
     handleScroll() {
@@ -22,6 +24,22 @@ export class NavbarComponent {
         } else {
             this.sticky = false;
         }
+    }
+
+    constructor(
+        private breakPointObserver: BreakpointObserver
+    ) {
+        this.breakpointObserverSubscription = breakPointObserver.observe([
+            ('(min-width: 960px)')
+        ]).subscribe((state: BreakpointState) => {
+            if (state.matches) {
+                this.navOpen = false;
+            }
+        })
+    }
+
+    ngOnDestroy() {
+        this.breakpointObserverSubscription.unsubscribe();
     }
 
     /**
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,21 +1,52 @@
-import { Component, Input, Output, EventEmitter, Sanitizer, SecurityContext } from '@angular/core';
+import {Component, Input, Output, EventEmitter, Sanitizer, SecurityContext, OnDestroy} from '@angular/core';
 import { Router } from '@angular/router';
+import {BreakpointObserver, BreakpointState} from '@angular/cdk/layout';
 
 @Component({
     selector: 'app-search',
     templateUrl: './search.component.html',
     styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {
+export class SearchComponent implements OnDestroy{
     @Output() close = new EventEmitter();
     @Output() searchSent = new EventEmitter(); // emit when we send a search
     @Input() placeholder = 'Search';
     searchText = '';
+    breakpointObserverSubscription: any;
+
+
 
     constructor(
         private router: Router,
-        private sanitizer: Sanitizer
-    ) {}
+        private sanitizer: Sanitizer,
+        private breakPointObserver: BreakpointObserver
+    ) {
+        this.breakpointObserverSubscription = breakPointObserver.observe([
+            ('(max-width: 960px)')
+        ]).subscribe((state: BreakpointState) => {
+            if (state.matches) {
+                this.close.emit();
+            }
+        })
+    }
+
+    ngOnDestroy() {
+        this.breakpointObserverSubscription.unsubscribe();
+    }
+
+    // @HostListener( 'window:resize', ['$event'] )
+    // onResize( event ): void {
+    //     this.screenWidth = window.innerWidth;
+    //     this.handleScreenSizeChange( this.screenWidth );
+    // }
+    //
+    // handleScreenSizeChange(screenSize: number) {
+    //     if (screenSize < 960) {
+    //         this.close.emit();
+    //     }
+    // }
+
+
 
     closeSearch() {
         this.searchText = '';
