7087c79a7e54d432ce0cb76f8e9b763f92d19f47
hule6187 <hunter.leise@lasp.colorado.edu>
2019-08-29 14:28:16 -0600

    Change app display to "contents". This will help fix the sticky nav bug in Safari once we dynamically pull in the new lasp-nav component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,8 @@ import {
 /** Entry Component */
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
 
