0156cfdab41b46ee2b331e9903a818fddbb36ca6
Elliot Whitehead <elliot.whitehead@lasp.colorado.edu>
2019-09-26 13:11:40 -0600

    Add additional images and messags to be iterated over randomly on page laod

--- src/app/four04-component/four04-component.component.ts
+++ src/app/four04-component/four04-component.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IGallery {
+  title: string;
+  imagePath: string;
+  text: string;
+};
+
 @Component({
   selector: 'lasp-four04-component',
   templateUrl: './four04-component.component.html',
   styleUrls: ['./four04-component.component.scss']
 })
 export class Four04ComponentComponent implements OnInit {
+  gallery: IGallery[];
 
-  constructor() { }
+  constructor() {
+    this.gallery = [
+      {
+        title : "Not Found",
+        imagePath : "assets/images/404/hubble1.jpg",
+        text : "Sorry, the page you're looking for seems to be lost in space."
+      },
+      {
+        title : "Hmm…",
+        imagePath : "assets/images/404/andromeda.jpg",
+        text : "Out of all the pages in the galaxy, we couldn't find that one. "
+      },
+      {
+        title : "Not Found",
+        imagePath : "assets/images/404/UGC_Cluster.jpg",
+        text : "Sorry, the page you're looking for seems to be lost in space."
+      }
+    ];
+  }
 
   ngOnInit() {
+    
   }
 
 }
