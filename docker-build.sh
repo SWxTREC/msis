8b65262646a2d2118a7b77bfd1996d3113749b14
Fernando Sanchez <fernando.sanchez@lasp.colorado.edu>
2019-11-13 09:23:23 -0700

    update docker scripts for correct capitalization

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
 REGISTRY=web-registry.lasp.colorado.edu
-NAME=Project-name
+NAME=project-name
 # have to check to make sure REFSPEC is bound / not empty for script to run
 if [[ ! -z ${REFSPEC+x} ]] && [ "${REFSPEC}" = "dev" ] ; then
 VERSION=dev
@@ -9,5 +9,6 @@ else
 VERSION="${VERSION:-$(npm version | head -n 1 | awk '{print $3;}' | tr -d "',")}"
 fi
 
+npm install
 ng build --prod --aot=false --build-optimizer=false --base-href '${NGINX_CONTEXT_ROOT}'
 docker build --force-rm -t ${REGISTRY}/${NAME}:${VERSION} .
