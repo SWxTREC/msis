d4615a8208fadb99e244933efe8273bee9e1836a
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2020-01-29 16:13:50 -0700

    add markdown parser and link to GitHub README.md

--- src/app/containers/docs/docs.module.ts
+++ src/app/containers/docs/docs.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { MaterialModule } from '../../modules';
 
@@ -10,6 +12,7 @@ import { DocsComponent } from './docs.component';
 @NgModule({
     imports: [
         CommonModule,
+        MarkdownModule.forRoot({ loader: HttpClient }),
         MaterialModule,
         DocsRoutingModule
     ],
