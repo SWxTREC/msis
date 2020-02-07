bce68af5b46aabd1fe9914595c0c19b54f3ca593
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2020-02-06 19:54:49 -0700

    new prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { version } from '../../package.json';
 
 export const environment = {
     // TODO: replace this with deployed vector API
-    vectorApi: 'https://eqpbpvy4u0.execute-api.us-east-1.amazonaws.com/api',
+    vectorApi: 'https://tqsgs1yg76.execute-api.us-east-1.amazonaws.com/prod/api',
     production: true,
     version: version
 };
