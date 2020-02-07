c5b4989649d8f3967c8f2b8ebf8e8e8c235a00bc
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2020-02-07 09:58:26 -0700

    go back to standard baseref, update in prod script, delete todo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 import { version } from '../../package.json';
 
 export const environment = {
-    // TODO: replace this with deployed vector API
     vectorApi: 'https://tqsgs1yg76.execute-api.us-east-1.amazonaws.com/prod/api',
     production: true,
     version: version
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-let baseHref: string;
 
 if ( environment.production ) {
     enableProdMode();
-    baseHref = '/vector/';
-} else {
-    baseHref = '/';
 }
 
 platformBrowserDynamic().bootstrapModule( AppModule )
