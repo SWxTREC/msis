b8234c189a2e8c7a253421b35af91452c042020d
Greg Lucas <greg.lucas@lasp.colorado.edu>
2020-07-18 14:16:14 -0600

    Updating the endpoint URL to the correct version for MSIS2.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { version } from '../../package.json';
 
 export const environment = {
-    msisApi: 'https://tqsgs1yg76.execute-api.us-east-1.amazonaws.com/prod/api',
+    msisApi: 'https://uqw5ak3cjh.execute-api.us-east-1.amazonaws.com/Prod/msis2',
     production: true,
     version: version
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ import { version } from '../../package.json';
 
 export const environment = {
     // TODO: replace this with actual url, this is for when running the flask app locally
-    msisApi: 'https://tqsgs1yg76.execute-api.us-east-1.amazonaws.com/prod/api',
+    msisApi: 'https://uqw5ak3cjh.execute-api.us-east-1.amazonaws.com/Prod/msis2',
     production: false,
     version: version
 };
