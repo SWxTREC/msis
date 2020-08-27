9dd5c0caa04d6aaba901256aae3ac7b5dd791d89
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2020-08-27 09:59:35 -0600

    linter update

--- src/app/services/model/model.service.ts
+++ src/app/services/model/model.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { _getOptionScrollPosition } from '@angular/material/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -12,32 +12,32 @@ export class ModelService {
     constructor(private http: HttpClient) { }
 
     submitSurfaceRequest(date: Date, altitude: number, f107: number, f107a: number,
-        ap: number, options: number[]) {
+                         ap: number, options: number[]) {
         return this.http.get<any>(environment.msisApi + '/surface', {
             params: {
-                "date": date.toISOString(), // might need to put in setSeconds(0) and milliseconds?
-                "altitude": altitude.toString(),
-                "f107": f107.toString(),
-                "f107a": f107a.toString(),
-                "ap": ap.toString(),
-                "options": options.toString()
+                date: date.toISOString(), // might need to put in setSeconds(0) and milliseconds?
+                altitude: altitude.toString(),
+                f107: f107.toString(),
+                f107a: f107a.toString(),
+                ap: ap.toString(),
+                options: options.toString()
             }
-        })
+        });
     }
 
     submitAltitudeRequest(date: Date, longitude: number, latitude: number,
-        f107: number, f107a: number,
-        ap: number, options: number[]) {
+                          f107: number, f107a: number,
+                          ap: number, options: number[]) {
         return this.http.get<any>(environment.msisApi + '/altitude', {
             params: {
-                "date": date.toISOString(), // might need to put in setSeconds(0) and milliseconds?
-                "longitude": longitude.toString(),
-                "latitude": latitude.toString(),
-                "f107": f107.toString(),
-                "f107a": f107a.toString(),
-                "ap": ap.toString(),
-                "options": options.toString()
+                date: date.toISOString(), // might need to put in setSeconds(0) and milliseconds?
+                longitude: longitude.toString(),
+                latitude: latitude.toString(),
+                f107: f107.toString(),
+                f107a: f107a.toString(),
+                ap: ap.toString(),
+                options: options.toString()
             }
-        })
+        });
     }
 }
