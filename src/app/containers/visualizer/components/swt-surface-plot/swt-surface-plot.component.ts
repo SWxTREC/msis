0e86442d40f23fc11d45a308a0ab7f3c8b4390ec
Greg Lucas <greg.lucas@lasp.colorado.edu>
2020-10-04 17:27:55 -0600

    ENH: Updating surface SVG attributes rather than redrawing
    - Refactored the geojson creation to create one feature collection that
      can be used to add data to the surface all at once.
    - The surface draw now uses the d3 -> selectAll -> .enter() methods
      for adding all the features into the `data` and then append all the
      paths to the cells.
    - Storing the surfaceCells and altitudeBox so that they can be updated
      without redrawing

--- src/app/containers/visualizer/components/swt-surface-plot/swt-surface-plot.component.ts
+++ src/app/containers/visualizer/components/swt-surface-plot/swt-surface-plot.component.ts
@@ -6,7 +6,7 @@ import { EARTH_MAP_URL, ISurfaceData } from 'src/app/models';
 @Component({
     selector: 'lasp-swt-surface-plot',
     templateUrl: './swt-surface-plot.component.html',
-    styleUrls: [ './swt-surface-plot.component.scss' ]
+    styleUrls: ['./swt-surface-plot.component.scss']
 })
 export class SwtSurfacePlotComponent implements OnChanges {
     @Input() data: ISurfaceData;
@@ -21,16 +21,27 @@ export class SwtSurfacePlotComponent implements OnChanges {
     hostElement: HTMLElement; // Native element hosting the SVG container
     svg: d3.Selection<SVGElement, {}, HTMLElement, any>; // Top level SVG element
     g: d3.Selection<SVGElement, {}, HTMLElement, any>; // SVG Group element
+    altitudeBox: d3.Selection<SVGElement, {}, HTMLElement, any>; // Altitude box
+    surfaceCells: d3.Selection<SVGElement, {}, SVGElement, any>; // All of the surface polygons
     surfaceColor: d3.ScaleSequential<string>;
     pathFromProjection: d3.GeoPath<any, d3.GeoPermissibleObjects>;
     projection: d3.GeoProjection;
 
-    constructor( private elRef: ElementRef ) {
+    constructor(private elRef: ElementRef) {
         this.hostElement = this.elRef.nativeElement;
     }
 
-    ngOnChanges() {
+    ngOnInit() {
         this.createSurfaceSvg();
+    }
+
+    ngOnChanges() {
+        // nGOnChanges is called before OnInit(), so if we have undefined
+        // items, just exit right away.
+        if (this.altitudeBox === undefined) {
+            return;
+        }
+        this.updateSurface();
     }
 
     addGraphicsElement() {
@@ -39,20 +50,19 @@ export class SwtSurfacePlotComponent implements OnChanges {
     }
 
     createSurfaceSvg(): void {
-        this.removeExistingChartFromParent();
         this.setChartDimensions();
         this.setColorScale();
         this.setProjection();
         this.addGraphicsElement();
         this.drawMap();
         this.drawSurface(this.data);
-        this.drawAltitudeBox();
     }
 
     drawAltitudeBox() {
-        const geoBox: d3.GeoPermissibleObjects = this.geoBoxFromPoint( this.longitude, this.latitude);
+        const geoBox: d3.GeoPermissibleObjects = this.geoBoxFromPoint(this.longitude, this.latitude);
         // draw a red box around the altitude profile location
-        this.g.append('path')
+        this.altitudeBox = this.g
+            .append('path')
             .attr('id', 'altitude-box')
             .attr('fill', 'none')
             .attr('stroke', 'red')
@@ -67,7 +77,7 @@ export class SwtSurfacePlotComponent implements OnChanges {
             .attr('fill', 'none')
             .attr('stroke', 'black');
 
-        d3.json( EARTH_MAP_URL ).then((data: any) => {
+        d3.json(EARTH_MAP_URL).then((data: any) => {
             this.g.append('g')
                 .append('path')
                 .attr('id', 'earth-map')
@@ -78,42 +88,83 @@ export class SwtSurfacePlotComponent implements OnChanges {
         });
     }
 
+    updateSurface() {
+        // Update the colorscale we are using
+        this.setColorScale();
+        // update the fill color of the surface cells
+        this.surfaceCells.attr('fill', (feature: any) => {
+            return this.surfaceColor(feature.properties[this.variable])
+        });
+        // update the location of the altitude box
+        this.altitudeBox.attr('d', this.pathFromProjection(this.geoBoxFromPoint(this.longitude, this.latitude)));
+    }
+
     drawSurface(data: any) {
+        const featureCollection = this.geoFeatureCollection(data);
+        this.surfaceCells = this.g.selectAll(".surface__cell")
+            .data(featureCollection.features)
+            .enter()
+            .append('path')
+            .attr('class', 'surface__cell')
+            .attr('d', this.pathFromProjection);
+
         const tooltip = this.g.append('text')
             .attr('class', 'surface__tooltip');
-        data[this.variable].map((value: number, i: number) => {
+        this.surfaceCells
+            .on('mouseover', (_: any, feature: any) => {
+                // mouseover returns the MouseEvent and then the feature as the second argument
+                const coordinates: [number, number] = [feature.properties.Longitude, feature.properties.Latitude];
+                const pixelCoordinates: [number, number] = this.projection(coordinates);
+                tooltip
+                    .attr('x', pixelCoordinates[0])
+                    .attr('y', pixelCoordinates[1])
+                    .attr('dx', '0.5rem')
+                    .attr('dy', '-0.5rem')
+                    .text(`(${feature.properties.Longitude.toFixed(0)}, ${feature.properties.Latitude.toFixed(0)})`);
+                tooltip.append('tspan')
+                    .attr('x', pixelCoordinates[0])
+                    .attr('y', pixelCoordinates[1])
+                    .attr('dx', '0.5rem')
+                    .attr('dy', '0.5rem')
+                    .text(() => `${this.variable}: ${feature.properties[this.variable].toExponential(3)} m`)
+                    .append('tspan')
+                    .attr('baseline-shift', 'super')
+                    .attr('font-size', '70%')
+                    .text('-3');
+            })
+            .on('click', (_: any, feature: any) => {
+                this.changeLocation.emit([feature.properties.Longitude, feature.properties.Latitude]);
+            });
+
+        // Add an altitude Box in
+        this.drawAltitudeBox();
+
+        // Update the attributes after drawing the initial areas
+        this.updateSurface();
+    }
+
+
+    geoFeatureCollection(data: any): any {
+        // Create an empty featureCollection that we can populate
+        var featureCollection = {
+            "name": "SurfacePolygons",
+            "type": "FeatureCollection",
+            "features": []
+        };
+
+        // Map over every point in the data and create a single
+        // Feature polygon
+        data['Longitude'].map((longitude: number, i: number) => {
             const latitude: number = data.Latitude[i];
-            const longitude: number = data.Longitude[i];
-            const coordinates: [number, number] = [ longitude, latitude ];
-            const geoBox: d3.GeoPermissibleObjects = this.geoBoxFromPoint(longitude, latitude);
-            this.g.append('path')
-                .attr('class', 'surface__cell')
-                .attr('fill', this.surfaceColor(value))
-                .style('opacity', 0.5)
-                .style('stroke-opacity', 0.5)
-                .attr('d', this.pathFromProjection(geoBox))
-                .on('mouseover', () => {
-                    tooltip
-                        .attr('x', this.projection(coordinates)[0])
-                        .attr('y', this.projection(coordinates)[1])
-                        .attr('dx', '0.5rem')
-                        .attr('dy', '-0.5rem')
-                        .text( `(${longitude.toFixed(0)}, ${latitude.toFixed(0)})` );
-                    tooltip.append('tspan')
-                        .attr('x', this.projection(coordinates)[0])
-                        .attr('y', this.projection(coordinates)[1])
-                        .attr('dx', '0.5rem')
-                        .attr('dy', '0.5rem')
-                        .text(() => `${this.variable}: ${value.toExponential(3)} m`)
-                        .append('tspan')
-                            .attr('baseline-shift', 'super')
-                            .attr('font-size', '70%')
-                            .text('-3');
-                })
-                .on('click', () => {
-                    this.changeLocation.emit([ longitude, latitude ]);
-                });
+            var feature: any = this.geoBoxFromPoint(longitude, latitude);
+            // For each feature polygon, add the data to the properties
+            Object.keys(data).map(key => {
+                feature.properties[key] = data[key][i];
+            });
+            // Add the feature into the list of features on the collection
+            featureCollection.features.push(feature);
         });
+        return featureCollection
     }
 
     geoBoxFromPoint(lon: number, lat: number): d3.GeoPermissibleObjects {
@@ -123,37 +174,24 @@ export class SwtSurfacePlotComponent implements OnChanges {
         const maxLat = clamp(lat + 2.5, -90, 90);
 
         return {
-            type: 'FeatureCollection',
-            features: [
-                {
-                    type: 'Feature',
-                    properties: {
-                        name: 'Rect'
-                    },
-                    geometry: {
-                        type: 'Polygon',
-                        coordinates: [ [ [ minLon, minLat ],
-                        [ minLon, maxLat ],
-                        [ maxLon, maxLat ],
-                        [ maxLon, minLat ],
-                        [ minLon, minLat ] ] ]
-                    }
-                }
-            ]
-        };
-    }
-
-    removeExistingChartFromParent() {
-        // !!!!Caution!!!
-        // Make sure not to do;
-        //     d3.select('svg').remove();
-        // That will clear all other SVG elements in the DOM
-        d3.select(this.hostElement).select('svg').remove();
+            type: 'Feature',
+            properties: {
+                name: 'Rect:' + lon + ',' + lat
+            },
+            geometry: {
+                type: 'Polygon',
+                coordinates: [[[minLon, minLat],
+                [minLon, maxLat],
+                [maxLon, maxLat],
+                [maxLon, minLat],
+                [minLon, minLat]]]
+            }
+        }
     }
 
     setChartDimensions() {
-        const viewBoxHeight = this.height + ( this.margin * 2 ) ;
-        const viewBoxWidth = this.width + ( this.margin * 2 );
+        const viewBoxHeight = this.height + (this.margin * 2);
+        const viewBoxWidth = this.width + (this.margin * 2);
         this.svg = d3.select(this.hostElement).append('svg')
             .attr('width', '100%')
             .attr('height', '100%')
@@ -162,13 +200,13 @@ export class SwtSurfacePlotComponent implements OnChanges {
 
     setColorScale() {
         this.surfaceColor = d3.scaleSequential(d3.interpolatePlasma)
-            .domain([ d3.min<number>(this.data[this.variable]), d3.max<number>(this.data[this.variable]) ]);
+            .domain([d3.min<number>(this.data[this.variable]), d3.max<number>(this.data[this.variable])]);
     }
 
     setProjection() {
         this.projection = d3.geoEqualEarth()
             .scale(187)
-            .translate([ this.width / 2, this.height / 2 ]);
+            .translate([this.width / 2, this.height / 2]);
         this.pathFromProjection = d3.geoPath(this.projection);
     }
 }
