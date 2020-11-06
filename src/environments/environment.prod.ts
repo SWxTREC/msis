a9c3c420bb197f28a71d4e0903107a2301a90255
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2020-11-06 14:01:39 -0700

    add slash to prod lisird latis path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { version } from '../../package.json';
 
 export const environment = {
     latisSwp: 'https://lasp.colorado.edu/space-weather-portal/latis/dap/',
-    latisLisird: 'https://lasp.colorado.edu/lisird/latis/dap',
+    latisLisird: 'https://lasp.colorado.edu/lisird/latis/dap/',
     msisApi: 'https://uqw5ak3cjh.execute-api.us-east-1.amazonaws.com/Prod/msis2',
     production: true,
     version: version
